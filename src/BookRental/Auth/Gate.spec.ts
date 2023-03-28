1bbcb777d87d37945cbaea259b3c6c4f71ce3929
Henrique Silverio <2057432+HenriqueSilverio@users.noreply.github.com>
2023-03-28 10:28:28 -0300

    test: add Gate unit tests

--- src/BookRental/Auth/Gate.spec.ts
+++ src/BookRental/Auth/Gate.spec.ts
@@ -1,4 +1,34 @@
+import JWT from 'jsonwebtoken'
+
+import IdentityObjectId from '../../BuildingBlocks/ValueObject/IdentityObjectId'
+
+import Password from '../User/Password'
+import User from '../User/User'
+
+import DecodedToken from './DecodedToken'
+import Gate from './Gate'
+
 describe('Gate', () => {
-  test.todo('Gate.signIn')
-  test.todo('Gate.verify')
+  const gate = new Gate('secret')
+
+  const user = new User(IdentityObjectId.create(), {
+    email: 'test@mail.com',
+    password: Password.fromHash('$2b$10$GQtDOkAybsuKHasCS99V7uPG86CHvmtn0hqFfIre6YIhbCAmm1PSC'),
+  })
+
+  test('Gate.signIn', async () => {
+    const token = await gate.signIn(user)
+
+    const decoded = JWT.decode(token) as DecodedToken
+
+    expect(decoded.userID).toBe(user.identity.props.uuid.valueOf())
+  })
+
+  test('Gate.verify', async () => {
+    const token = await gate.signIn(user)
+
+    const verified = await gate.verify(token) as DecodedToken
+
+    expect(verified.userID).toBe(user.identity.props.uuid.valueOf())
+  })
 })
